80c58cb9f9a69809886759e84f3b6cc9105009d3
Alexandra Balo <alexandrabalo@yahoo.com>
2025-04-30 12:53:58 +0300

    update connection to db

--- db.js
+++ db.js
@@ -1,20 +1,8 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
-// });
-
-// module.exports = pool;
-
 require('dotenv').config();
-const postgres = require('postgres'); 
+const postgres = require('postgres');
 
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+const sql = postgres(process.env.DATABASE_URL, {
+  ssl: 'require'
+});
 
 module.exports = sql;
